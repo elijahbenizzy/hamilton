8ded2aec898c5541118eff046f56d5cd0cb955cb
elijah.benizzy <elijah.benizzy@stitchfix.com>
2021-12-08 12:40:25 -0800

    Implements opinionated decorator lifecycle
    This adds to the set of decorators that can be applied to a node,
    and puts them in a uniform setting. The ordering is provided in
    function_base.resolve_nodes -- the algorithm is as follows:
    
    1. If there is a list of function resolvers, apply them one
    after the other. Otherwise, apply the default function resolver
    which will always return just the function. This determines whether to
    proceed -- if any function resolver is none, short circuit and return
    an empty list of nodes.
    
    2. If there is a list of node creators, that list must be of length 1
    -- this is determined in the node creator class. Apply that to get
    the initial node.
    
    3. If there is a list of node expanders, apply them. Otherwise apply the default
    nodeexpander This must be a
    list of length one. This gives out a list of nodes.
    
    4. If there is a node transformer, apply that. Note that the node transformer
    gets applied individually to just the sink nodes in the subdag. It subclasses
    "DagTransformer" to do so.
    
    5. Return the final list of nodes.

--- hamilton/graph.py
+++ hamilton/graph.py
@@ -8,7 +8,7 @@ import logging
 from types import ModuleType
 from typing import Type, Dict, Any, Callable, Tuple, Set, Collection, List
 
-from hamilton import function_modifiers
+import hamilton.function_modifiers_base
 from hamilton import node
 from hamilton.node import NodeSource, DependencyType
 
@@ -50,6 +50,7 @@ def add_dependency(
     if param_name in nodes:
         # validate types match
         required_node = nodes[param_name]
+        print(required_node.type, param_type)
         if param_type == dict and issubclass(required_node.type, Dict):  # python3.7 changed issubclass behavior
             pass
         elif param_type == list and issubclass(required_node.type, List):  # python3.7 changed issubclass behavior
@@ -79,9 +80,9 @@ def create_function_graph(*modules: ModuleType, config: Dict[str, Any]) -> Dict[
 
     # create nodes -- easier to just create this in one loop
     for func_name, f in functions:
-        for n in function_modifiers.resolve_nodes(f, config):
+        for n in hamilton.function_modifiers_base.resolve_nodes(f, config):
             if n.name in config:
-                continue # This makes sure we overwrite things if they're in the config...
+                continue  # This makes sure we overwrite things if they're in the config...
             if n.name in nodes:
                 raise ValueError(f'Cannot define function {n.name} more than once.'
                                  f' Already defined by function {f}')
@@ -197,6 +198,7 @@ class FunctionGraph(object):
     def directional_dfs_traverse(self, next_nodes_fn: Callable[[node.Node], Collection[node.Node]], starting_nodes: List[str]):
         nodes = set()
         user_nodes = set()
+
         def dfs_traverse(node: node.Node):
             for n in next_nodes_fn(node):
                 if n not in nodes:
@@ -238,7 +240,7 @@ class FunctionGraph(object):
         if computed is None:
             computed = {}
 
-        def dfs_traverse(node: node.Node, dependency_type: DependencyType=DependencyType.REQUIRED):
+        def dfs_traverse(node: node.Node, dependency_type: DependencyType = DependencyType.REQUIRED):
             for n in node.dependencies:
 
                 if n.name not in computed:
